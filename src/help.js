6747aeebd411da9c355b22264c996b4ea557e15c
najsisuwnwnkamsjsjsiwkamnshzhjsksuxwbct <76475667+najsisuwnwnkamsjsjsiwkamnshzhjsksuxwbct@users.noreply.github.com>
2020-12-31 23:41:39 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,191 @@
+
+const help = (prefix, pushname) => { 
+	return `𝐍𝐎𝐄𝐄𝐑𝐁𝐎𝐓       
+	     
+	Hallo ${pushname}
+	
+┏━━━━°❀ ❬ 𝘼𝘽𝙊𝙐𝙏 ❭ ❀°━━━━┓
+┃╔═══════════════════╗
+┏❉ *${prefix}owner*
+┣❉ *${prefix}donasi*
+┗❉ *${prefix}info*
+┃╚═══════════════════╝
+┣━━━━°❀ ❬ 𝗠𝗔𝗞𝗘𝗥 ❭ ❀°━━━━⊱
+┃╔═══════════════════╗
+┃╠➥ *${prefix}sticker*
+┃╠➥ *${prefix}sticker nobg*
+┃╠➥ *${prefix}tsticker*
+┃╠➥ *${prefix}nulis*
+┃╠➥ *${prefix}ttp*
+┃╠➥ *${prefix}glitch*
+┃╠➥ *${prefix}logoneon*
+┃╠➥ *${prefix}logowolf1*
+┃╠➥ *${prefix}logowolf2*
+┃╠➥ *${prefix}logojoker*
+┃╠➥ *${prefix}logolion*
+┃╠➥ *${prefix}textscreen*
+┃╠➥ *${prefix}tahta*
+┃╠➥ *${prefix}rtext*
+┃╠➥ *${prefix}party*
+┃╠➥ *${prefix}lovemake*
+┃╠➥ *${prefix}pronlogo*
+┃╠➥ *${prefix}water*
+┃╠➥ *${prefix}epep*
+┃╠➥ *${prefix}text3d*
+┃╠➥ *${prefix}quotemaker*
+┃╚═══════════════════╝
+┣━━━━°❀ ❬ 𝙈𝙀𝘿𝙄𝘼 ❭ ❀°━━━━━⊱
+┃╔═══════════════════╗
+┃╠➥ *${prefix}tts*
+┃╠➥ *${prefix}tiktok*
+┃╠➥ *${prefix}meme*
+┃╠➥ *${prefix}memeindo*
+┃╠➥ *${prefix}nsfwloli* 
+┃╠➥ *${prefix}nsfwtrp*
+┃╠➥ *${prefix}nsfwnek*
+┃╠➥ *${prefix}hentai*
+┃╠➥ *${prefix}ocr*
+┃╠➥ *${prefix}neko*
+┃╠➥ *${prefix}ranime*
+┃╠➥ *${prefix}loli*
+┃╠➥ *${prefix}waifu*
+┃╠➥ *${prefix}tomp3*
+┃╠➥ *${prefix}walpaperhd*
+┃╠➥ *${prefix}mlherolist*
+┃╠➥ *${prefix}pinterest <query>*
+┃╠➥ *${prefix}inu*
+┃╠➥ *${prefix}pokemon*
+┃╠➥ *${prefix}elang*
+┃╠➥ *${prefix}naruto*
+┃╠➥ *${prefix}minato*
+┃╠➥ *${prefix}sakura*
+┃╠➥ *${prefix}boruto*
+┃╠➥ *${prefix}hinata*
+┃╠➥ *${prefix}sasuke*
+┃╠➥ *${prefix}unta*
+┃╚═══════════════════╝
+┣━━━°❀ ❬ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 ❭ ❀°━━⊱
+┃╔═══════════════════╗
+┃╠➥ *${prefix}ytmp3*
+┃╠➥ *${prefix}ytmp4*
+┃╠➥ *${prefix}ytsearch*
+┃╠➥ *${prefix}joox*
+┃╚═══════════════════╝
+┣━━━━°❀ ❬ 𝙂𝙍𝙊𝙐𝙋 ❭ ❀°━━━━⊱
+┃╔═══════════════════╗
+┃╠➥ *${prefix}add* [62xxx]
+┃╠➥ *${prefix}kick* [tag]
+┃╠➥ *${prefix}leave*
+┃╠➥ *${prefix}out*
+┃╠➥ *${prefix}linkgc*
+┃╠➥ *${prefix}setpp*
+┃╠➥ *${prefix}tagme*
+┃╠➥ *${prefix}demote* [tag]
+┃╠➥ *${prefix}promote* [tag]
+┃╠➥ *${prefix}grup* [buka/tutup]
+┃╠➥ *${prefix}welcome* [1/0]
+┃╠➥ *${prefix}nsfw* [1/0]
+┃╠➥ *${prefix}simih* [1/0]
+┃╠➥ *${prefix}hidetag*
+┃╠➥ *${prefix}infogc*
+┃╠➥ *${prefix}groupinfo*
+┃╠➥ *${prefix}setdesc*
+┃╠➥ *${prefix}delete*
+┃╚═══════════════════╝
+┣━━━━°❀ ❬ 𝙊𝙒𝙉𝙀𝙍 ❭ ❀°━━━━⊱
+┃╔═══════════════════╗
+┃╠➥ *${prefix}bc* 
+┃╠➥ *${prefix}leave*
+┃╠➥ *${prefix}clearall*
+┃╠➥ *${prefix}setprefix*
+┃╠➥ *${prefix}leave*
+┃╠➥ *${prefix}clone* [tag]
+┃╠➥ *${prefix}hidetag*
+┃╠➥ *${prefix}setppbot*
+┃╚═══════════════════╝
+┣━━━━━°❀ ❬ 𝙎𝙋𝘼𝙈 ❭ ❀°━━━━━⊱
+┃╔═══════════════════╗
+┃╠➥ *${prefix}spamsms*
+┃╠➥ *${prefix}spamcall*
+┃╠➥ *${prefix}spamgmail*
+┃╚═══════════════════╝
+┣━━━━°❀ ❬ 𝙊𝙏𝙃𝙀𝙍 ❭ ❀°━━━━⊱
+┃╔═══════════════════╗
+┃╠➥ *${prefix}listadmin*
+┃╠➥ *${prefix}blocklist*
+┃╠➥ *${prefix}simi*
+┃╠➥ *${prefix}wait*
+┃╠➥ *${prefix}nama*
+┃╠➥ *${prefix}map*
+┃╠➥ *${prefix}cerpen*
+┃╠➥ *${prefix}qrcode*
+┃╠➥ *${prefix}fitnah*
+┃╠➥ *${prefix}tiktokstalk*
+┃╠➥ *${prefix}shortlink*
+┃╠➥ *${prefix}url2img*
+┃╠➥ *${prefix}alay*
+┃╠➥ *${prefix}lirik*
+┃╠➥ *${prefix}quotes*
+┃╠➥ *${prefix}bucin*
+┃╠➥ *${prefix}dewabatch*
+┃╠➥ *${prefix}corona*
+┃╠➥ *${prefix}wiki*
+┃╠➥ *${prefix}wikien*
+┃╠➥ *${prefix}zodiak* 
+┃╠➥ *${prefix}kbbi*
+┃╠➥ *${prefix}gay*
+┃╠➥ *${prefix}igstalk*
+┃╠➥ *${prefix}infogempa*
+┃╠➥ *${prefix}ping*
+┃╠➥ *${prefix}tebakgambar*
+┃╠➥ *${prefix}caklontong*
+┃╠➥ *${prefix}family100*
+┃╠➥ *${prefix}game*
+┃╠➥ *${prefix}primbonjodoh*
+┃╠➥ *${prefix}artinama*
+┃╠➥ *${prefix}testime*
+┃╠➥ *${prefix}apakah*
+┃╠➥ *${prefix}kapankah*
+┃╠➥ *${prefix}hobby*
+┃╠➥ *${prefix}rate*
+┃╠➥ *${prefix}watak*
+┃╠➥ *${prefix}bisakah*
+┃╠➥ *${prefix}truth*
+┃╠➥ *${prefix}dare*
+┃╠➥ *${prefix}ramalhp*
+┃╠➥ *${prefix}image*
+┃╠➥ *${prefix}infocuaca*
+┃╠➥ *${prefix}anjing*
+┃╠➥ *${prefix}indohot*
+┃╠➥ *${prefix}resepmasakan*
+┃╠➥ *${prefix}infonomor*
+┃╠➥ *${prefix}infomobil*
+┃╠➥ *${prefix}infomotor*
+┃╠➥ *${prefix}ceckjodoh*
+┃╠➥ *${prefix}playstore*
+┃╚═══════════════════╝
+┣━━━━°❀ ❬ 𝙎𝙊𝙐𝙉𝘿 ❭ ❀°━━━━⊱
+┃╔═══════════════════╗
+┃╠➥ *${prefix}tariksis*
+┃╠➥ *${prefix}onidah*
+┃╠➥ *${prefix}salam*
+┃╠➥ *${prefix}baka*
+┃╠➥ *${prefix}welot*
+┃╠➥ *${prefix}abangjago*
+┃╠➥ *${prefix}iri*
+┃╠➥ *${prefix}tapi*
+┃╚═══════════════════╝
+┣━━━━━━━━━━━━━━━━━━━━━⊱
+┃┌───────────────────┐
+┃│╔══╗╔═╗╔══╗╔══╗
+┃│║╔╗║║╬║╚║║╝║══╣
+┃│║╠╣║║╗╣╔║║╗╠══║
+┃│╚╝╚╝╚╩╝╚══╝╚══╝
+┃├───────────────────┐
+┃└───────────────────┘
+┣━━━━━━━━━━━━━━━━━━━━━┓
+┃  𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗔𝗥𝗜𝗦𝟭𝟴𝟳 𝗜𝗗   
+┗━━━━━━━━━━━━━━━━━━━━━┛`
+}
+
+exports.help = help
